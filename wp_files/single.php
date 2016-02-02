24aca5aebdff2d51c759a2d25d0180ce76bf408f
yaphi1 <yaphi1@gmail.com>
2016-02-02 15:01:21 -0500

    added levels and numbers, and added sublime link

--- wp_files/single.php
+++ wp_files/single.php
@@ -1,12 +1,32 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-<?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
-</section>
-<?php get_sidebar(); ?>
+
+<div id="content">
+	<div id="maincol">
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		<?php
+		$currentCategory = get_the_category();
+		if ($currentCategory[0]->name == 'Lessons'){
+			$currentID = get_the_ID();
+			$currentNumber = Get_Post_Number($currentID, $currentCategory[1]->name);
+			?>
+			<br><?php echo $currentCategory[1]->name; ?>
+			- Unit <?php echo $currentNumber; ?>
+		<?php } ?>
+
+
+		<h1><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+
+		<?php echo get_post_meta($post->ID, "scripts", true); ?>
+		<?php endwhile; endif; ?>
+
+		<?php get_template_part( 'nav', 'below-single' ); ?>
+	</div>
+
+	<?php get_sidebar(); ?>
+</div>
+
 <?php get_footer(); ?>
