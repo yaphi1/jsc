7e673ab96fc80202868fe2413042feefbd4036a5
yaphi1 <yaphi1@gmail.com>
2016-07-18 15:42:37 -0400

    tues email

--- outreach/mailchimp/get_mailchimp_emails.js
+++ outreach/mailchimp/get_mailchimp_emails.js
@@ -1,1 +1,8 @@
 Array.slice.call(null, document.querySelectorAll('.profile-view a')).forEach(function(v){console.log(v.innerHTML)});
+
+
+
+
+chrome:
+
+Array.prototype.slice.call(document.querySelectorAll('.profile-view a')).forEach(function(v){console.log(v.innerHTML);});
