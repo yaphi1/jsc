4a97a257c7d303c7cb508f6ce5e7ce2a6bf7132b
yaphi1 <yaphi1@users.noreply.github.com>
2016-08-04 23:23:49 -0400

    started param draft 4

--- outreach/sitepoint/url_parameters/url_parameters_test_code.js
+++ outreach/sitepoint/url_parameters/url_parameters_test_code.js
@@ -5,45 +5,68 @@
 // latest version
 
 function getAllUrlParams(url){
-	var s = url ? url.split('?')[1] : window.location.search.substr(1);
-	if(s) {
+	
+	// get query string from url (optional) or window
+	var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+	
+	// we'll store the parameters here
+	var obj = {};
+
+	// if query string exists
+	if(queryString) {
+		
+		// stuff after # is not part of query string, so get rid of it
+		queryString = queryString.split('#')[0];
+
 		// split our query string into its component parts
-		var arr = s.split('&');
-		var obj = {};
+		var arr = queryString.split('&');
+
 		for(var i=0; i<arr.length; i++) {
 			// separate the keys and the values
 			var a = arr[i].split('=');
 			
 			// in case params look like: list[]=thing1&list[]=thing2
-			var paramName = a[0].replace('[]','');
+			var paramNum = undefined;
+			var paramName = a[0].replace(/\[\d*\]/, function(v){
+				paramNum = v.slice(1,-1);
+				return '';
+			});
 
 			// set parameter value (use 'true' if empty)
 			var paramValue = typeof(a[1])==='undefined' ? true : a[1];
 			
 			// if parameter name already exists
 			if(obj[paramName]){
-				// convert to array (if still string)
+				// convert value to array (if still string)
 				if(typeof obj[paramName] === 'string'){
-					obj[paramName] = obj[paramName].split();
+					obj[paramName] = [obj[paramName]];
 				}
-				// add latest item to array
-				obj[paramName].push(paramValue);
+				// if no array index number specified...
+				if(typeof paramNum === 'undefined'){
+					// put the value on the end of the array
+					obj[paramName].push(paramValue);
+				}
+				// if array index number specified...
+				else{
+					// put the value at that index number
+					obj[paramName][paramNum] = paramValue;
+				}
 			}
 			// if param name doesn't exist yet, set it
 			else{
 				obj[paramName] = paramValue;
 			}
 		}
-		return obj;
 	}
-	else {
-		return {};
-	}
+
+	return obj;
 }
 
 getAllUrlParams('http://example.com/?a=1&b=adsf&c=&d&whatever&abc=1');
 
+getAllUrlParams('http://example.com/?a=1&a=adsf&a=&d&whatever&abc=1');
 
+getAllUrlParams('http://example.com/?a[0]=1&a[2]=adsf&a[]=&d&whatever&abc=1');
 
 
 /*
