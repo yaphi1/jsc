fb81418dda5500f427dd42a0d1cbc75832477fa0
yaphi1 <yaphi1@gmail.com>
2016-08-05 11:46:48 -0400

    finished draft 4

--- outreach/sitepoint/url_parameters/url_parameters_test_code.js
+++ outreach/sitepoint/url_parameters/url_parameters_test_code.js
@@ -34,6 +34,10 @@ function getAllUrlParams(url){
 
 			// set parameter value (use 'true' if empty)
 			var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+			// (optional) keep case consistent
+			paramName = paramName.toLowerCase();
+			paramValue = paramValue.toLowerCase();
 			
 			// if parameter name already exists
 			if(obj[paramName]){
