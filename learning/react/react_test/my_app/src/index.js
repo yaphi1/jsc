2718404704be804f63e81af25d642b370c4b7f94
yaphi1 <yaphi1@gmail.com>
2017-08-15 17:11:22 -0400

    got up to state in react

--- learning/react/react_test/my_app/src/index.js
+++ learning/react/react_test/my_app/src/index.js
@@ -6,11 +6,73 @@ import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
+
+const profileData = [
+	{id:'001', name:'alice', city:'washington'},
+	{id:'002', name:'bob', city:'sacramento'},
+	{id:'003', name:'calvin', city:'chicago'}
+];
+
+function Profile(props){
+	return (
+		<div className="profile">
+			<div>Name: {props.name}</div>
+			<div>City: {props.city}</div>
+			<br />
+		</div>
+	);
+}
+
+const allProfiles = profileData.map((v,i)=>{
+	return (
+		<Profile key={v.id} name={v.name} city={v.city} />
+	);
+});
+
+
+class Clock extends React.Component {
+	render(){
+		return (
+			<div>{new Date().toLocaleTimeString()}</div>
+		);
+	}
+}
+
+
+function ActionLink(){
+	function handleClick(e){
+		e.preventDefault();
+		console.log('link was clicked');
+	}
+
+	return (
+		<a href="#" onClick={handleClick}>Click this</a>
+	);
+}
+
+
+
+const payload = (
+	<div>
+		<h1>Profile list</h1>
+		{allProfiles}
+		Clock: <Clock />
+		<ActionLink />
+	</div>
+);
+
+// const testProfile = <Profile name="TestName" city="TestCity" />;
+
+/*const payload = (
 	<div>
 		<App />
 		<h1>Hello world!</h1>
-	</div>,
+		{allProfiles}
+	</div>
+);*/
+
+ReactDOM.render(
+	payload,
 	document.getElementById('root')
 );
 
